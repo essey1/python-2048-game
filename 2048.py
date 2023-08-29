66a49f999dd05d66846c1e1647db01c7b2e4e010
essey1 <esseyt3@gmail.com>
2023-08-29 15:36:57 +0300

    set up the main game window and handled keyboard events

--- 2048.py
+++ 2048.py
@@ -52,3 +52,28 @@ CELL_NUMBER_FONTS = {
     1024: ("Helvetica", 40, "bold"),
     2048: ("Helvetica", 40, "bold")
 }
+
+# 
+class Game(tk.Frame):
+    def __init__(self):
+        tk.Frame.__init__(self)
+        self.grid()
+        self.master.title('2048')
+
+        self.main_grid = tk.Frame(
+            self, 
+            bg=GRID_COLOR, 
+            bd=3, width=400, 
+            height=400)
+        self.main_grid.grid(pady=(80, 0))
+        self.make_GUI()
+        self.start_game()
+
+        self.master.bind("<Left>", self.left)
+        self.master.bind("<Right>", self.right)
+        self.master.bind("<Up>", self.up)
+        self.master.bind("<Down>", self.down)
+
+        
+
+    
